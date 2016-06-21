81bfa25422bc5befc17f219445c22b930ece51e2
xuzhiwei1989 <1351033315@qq.com>
2016-06-21 21:23:12 +0800

    5.5.3

---
+++ Book5.5.3/Book5.5.3/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Book5._5._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.tbFileName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbExtension = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.Location = new System.Drawing.Point(59, 37);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.Size = new System.Drawing.Size(213, 21);
+            this.tbFilePath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文件";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(59, 65);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(213, 21);
+            this.tbPath.TabIndex = 2;
+            // 
+            // tbFileName
+            // 
+            this.tbFileName.Location = new System.Drawing.Point(59, 93);
+            this.tbFileName.Name = "tbFileName";
+            this.tbFileName.Size = new System.Drawing.Size(100, 21);
+            this.tbFileName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "文件路径";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "文件名";
+            // 
+            // tbExtension
+            // 
+            this.tbExtension.Location = new System.Drawing.Point(59, 121);
+            this.tbExtension.Name = "tbExtension";
+            this.tbExtension.Size = new System.Drawing.Size(100, 21);
+            this.tbExtension.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "扩展名";
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(84, 172);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(75, 23);
+            this.btnSplit.TabIndex = 8;
+            this.btnSplit.Text = "分离";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbExtension);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbFileName);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFilePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.TextBox tbFileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbExtension;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSplit;
+    }
+}
+
---
+++ Book5.5.3/Book5.5.3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book5._5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            string filePath = tbFilePath.Text.Trim();
+
+            string[] filePathArr = filePath.Split(new char[] { '\\' });
+            //tbExtension.Text = filePathArr[filePathArr.Length - 1];
+            string path = null;
+            for (int i = 0; i < filePathArr.Length - 2;i++ )
+            {
+                if (i > 0)
+                {
+                    path += "\\";
+                }
+                path += filePathArr[i];
+            }
+
+            string fileName = filePathArr[filePathArr.Length - 1];
+            string[] fileNameArr = fileName.Split(new char[] { '.' });
+            string name = null;
+            int j = 0;
+            do
+            {
+                if (j > 0)
+                {
+                    name += ".";
+                }
+                name += fileNameArr[j++];
+            } while (j < fileNameArr.Length - 1);
+
+            //tbFilePath.Text = path;
+            tbPath.Text = path;
+            tbFileName.Text = name;
+            tbExtension.Text = "." + fileNameArr[fileNameArr.Length - 1];
+
+        }
+    }
+}
