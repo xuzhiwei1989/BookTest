db86b857198692b1f46f212e4e8ba42b9a6d7c74
xuzhiwei1989 <1351033315@qq.com>
2016-06-21 22:49:35 +0800

    第6章练习题

---
+++ Book6.5.1/Book6.5.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book6._5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a number:");
+            string input = Console.ReadLine().Trim();
+            int num;
+            int.TryParse(input, out num);
+            if (num > 6)
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("The number is lower than 6....");
+            }
+        }
+    }
+}
---
+++ Book6.5.2/Book6.5.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book6._5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ball = new int[] { 10, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+            List<int> balls = Test(ball);
+
+            foreach (int i in balls)
+            {
+                Console.Write(ball[i] + " ");
+            }
+        }
+
+        public static List<int> Test(int[] ballArray)
+        {
+            List<int> res = new List<int>();
+            Random r = new Random();
+            int randomNum;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    GetRandomNum:
+            //        randomNum = r.Next(10);
+
+            //    if (res.Contains(randomNum))
+            //    {
+            //        goto GetRandomNum;
+            //    }
+            //    else
+            //    {
+            //        res.Add(randomNum);
+            //    }
+            //}
+            while (res.Count < 5)
+            {
+                randomNum = r.Next(10);
+                if (!res.Contains(randomNum))
+                {
+                    res.Add(randomNum);
+                }
+            }
+
+            return res;
+        }
+    }
+}
