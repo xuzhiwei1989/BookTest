a121003483b4b3f128feff5a897e528e5d9966e8
xuzhiwei1989 <1351033315@qq.com>
2016-06-24 17:41:30 +0800

    第7章练习题 希尔排序法

---
+++ Book7.8.1ShellSort/Book7.8.1ShellSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book7._8._1ShellSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            int[] arr = { 1, 8, 9, 4, 3, 6, 2, 10, 70, 5, 90, 40, 0 };
+            //int[] res = pro.ShellSort(arr, arr.Length);
+            pro.ShellSort(arr);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void ShellSort(int[] arr)
+        {
+            int d = arr.Length / 2;
+            while (d >= 1)
+            {
+                ShellInsertSort(arr, d);
+                d = d / 2;
+            }
+        }
+
+        public void ShellInsertSort(int[] arr,int d)
+        {
+            //int[] res = arr;
+            for (int i = d; i < arr.Length; i++)
+            {
+                if (arr[i] < arr[i - d])
+                {
+                    int j = i - d;
+                    int x = arr[i];
+                    while (j >= 0 && x < arr[j])
+                    {
+                        arr[j + d] = arr[j];
+                        j -= d;
+                    }
+                    arr[j + d] = x;
+                }
+            }
+
+            //return res;
+        }
+    }
+}
