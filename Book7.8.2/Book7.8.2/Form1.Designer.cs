fa921992ef86fbd42bc560611e6d940bc45d1f3a
xuzhiwei1989 <1351033315@qq.com>
2016-06-25 18:07:43 +0800

    书第7章第二题习题

---
+++ Book7.8.2/Book7.8.2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Book7._8._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbStr = new System.Windows.Forms.TextBox();
+            this.tbNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbStr
+            // 
+            this.tbStr.Location = new System.Drawing.Point(95, 45);
+            this.tbStr.Name = "tbStr";
+            this.tbStr.Size = new System.Drawing.Size(149, 21);
+            this.tbStr.TabIndex = 0;
+            // 
+            // tbNum
+            // 
+            this.tbNum.Location = new System.Drawing.Point(95, 86);
+            this.tbNum.Name = "tbNum";
+            this.tbNum.ReadOnly = true;
+            this.tbNum.Size = new System.Drawing.Size(149, 21);
+            this.tbNum.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "字符串：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "汉字数量：";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(95, 136);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 4;
+            this.btnGet.Text = "识别";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNum);
+            this.Controls.Add(this.tbStr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbStr;
+        private System.Windows.Forms.TextBox tbNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGet;
+    }
+}
+
---
+++ Book7.8.2/Book7.8.2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Book7._8._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int GetNum(string str)
+        {
+            //int i = 0;
+            //foreach (char ch in str)
+            //{
+            //    if (ch >= 0x4e00 && ch <= 0x9fa5)
+            //    {
+            //        i++;
+            //    }
+            //}
+
+            //return i;
+
+            //正则表达式
+            ArrayList arrList = new ArrayList();
+            CharEnumerator CEnumerator = str.GetEnumerator();
+            Regex regex = new Regex("^[\u4E00-\u9FA5]$");
+            while (CEnumerator.MoveNext())
+            {
+                if (regex.IsMatch(CEnumerator.Current.ToString(), 0))
+                {
+                    arrList.Add(CEnumerator.Current.ToString());
+                }
+            }
+
+            return arrList.Count;
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string str = tbStr.Text;
+            tbNum.Text = GetNum(str).ToString();
+        }
+    }
+}
