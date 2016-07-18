0c4aa6b8f612a722b0b201403ef64479cbaf53c9
xuzhiwei1989 <1351033315@qq.com>
2016-07-18 20:54:20 +0800

    book
    book

---
+++ BookTest10.2.3/BookTest10.2.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTest10._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveClass driveClass = new DriveClass();
+            driveClass.ID = "00123";
+            driveClass.ShowMessage();
+
+            myClass myclass = driveClass;
+            myclass.ID = "00124";
+            myclass.ShowMessage();
+            driveClass.ShowMessage();
+        }
+    }
+
+    public abstract class myClass
+    {
+        private string _id = "";
+
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public abstract void ShowMessage();
+    }
+
+    public class DriveClass : myClass
+    {
+        public override void ShowMessage()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine(ID + " ");
+        }
+    }
+}
