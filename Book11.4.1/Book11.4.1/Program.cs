438efd50f6dbbdd880e76dd95a80eb1c339af543
xuzhiwei1989 <1351033315@qq.com>
2016-07-25 22:02:32 +0800

    Book第11章练习

---
+++ Book11.4.1/Book11.4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace Book11._4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Try to connect SQL Server...");
+                SqlConnection sqlConn = new SqlConnection("server=localhost;database=db;uid=sa;pwd=123");
+                sqlConn.Open();
+                if (sqlConn.State == System.Data.ConnectionState.Open)
+                {
+                    Console.WriteLine("Connect SQL Server Succeed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Book11.4.2/Book11.4.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book11._4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1;
+                bool b = true;
+                int c = a + b;
+                Console.WriteLine(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
