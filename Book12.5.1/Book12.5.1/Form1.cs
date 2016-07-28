cfdefed866143f5170ee518d65cb0f323f1849b8
xuzhiwei1989 <1351033315@qq.com>
2016-07-28 22:44:33 +0800

    Book第12章练习

---
+++ Book12.5.1/Book12.5.1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book12._5._1
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.timer = new Timer();
+            this.timer.Enabled = true;
+            this.timer.Interval = 500;
+
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.Opacity += 0.1;
+        }
+    }
+}
